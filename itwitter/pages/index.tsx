717d30e0032759436cd54709c4caf032954f520d
sandeepsingh61935 <sandeepss128961@gmail.com>
2023-05-20 17:56:23 +0530

    feat: sidebar is added

--- itwitter/pages/index.tsx
+++ itwitter/pages/index.tsx
@@ -1,8 +1,11 @@
+import Sidebar from "@/components/Sidebar"
 
 export default function Home() {
   return (
-   <div className="text-3xl text-sky-500">
-      Hellow world!
+    <div className="h-screen bg-black">
+     <div className="grid grid-cols-4 h-full">
+      <Sidebar />
+     </div>
     </div>
   )
 }
