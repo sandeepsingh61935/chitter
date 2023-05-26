782aca803f63d570fce941c76408d9513510788a
sandeepsingh61935 <sandeepss128961@gmail.com>
2023-05-26 15:27:50 +0530

    remove a typo error

--- chitter/pages/api/follow.ts
+++ chitter/pages/api/follow.ts
@@ -23,7 +23,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       }
     });
 
-    if (!user) {f
+    if (!user) {
       throw new Error('Invalid ID');
     }
 
