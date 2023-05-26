578030f991ec5f8e52f4b772ce2d49ec1bb05d73
sandeepsingh61935 <sandeepss128961@gmail.com>
2023-05-26 15:05:11 +0530

    feat: notification feature is done

--- chitter/pages/_app.tsx
+++ chitter/pages/_app.tsx
@@ -6,11 +6,13 @@ import "@/styles/globals.css";
 import { Toaster } from 'react-hot-toast';
 import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from "next/app";
+import EditModal from "@/components/modals/EditModal";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <Toaster />
+      <EditModal/>
       <RegisterModal />
       <LoginModal />
       <Layout>
